7c21dfc6085bcbfd76a154d5417940632fc59baa
dev-mev <40147757+dev-mev@users.noreply.github.com>
2018-08-25 19:44:33 -0700

    set up crystal game

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// generate random number (19-120)
+// generate random numbers for each crystal (1-12)
+// on click add value of crystal to total score
+// when total score equals random number increment wins
+// when total score goes over random number increment losses
+// after win or loss happens reset the game (new random number, new values on crystals, total score equals 0)
